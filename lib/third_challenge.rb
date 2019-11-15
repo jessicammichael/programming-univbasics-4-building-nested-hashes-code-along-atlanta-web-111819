9be35bfd93bae708b3754c0ec33da8826ccc0a3f
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 04:53:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,21 +5,13 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montage", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "Alive"},
-      :hero_friends => []
-   },
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => {[name: "Benvolio", attitude: "worried"] [name:"Mercutio", age: "18", attitude: "hot-headed"]},
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => {[name: "Steven", age: "30", attitude: "confused"] [name: "Nurse", age: "44", attitude: "worried"]}
   }
 
 
